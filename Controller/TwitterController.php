27a0c312be0df239af2e59ac9a05eab1491f5cb6
Thomas Jarrand <thomas.jarrand@gmail.com>
2012-09-15 22:36:38 +0800

    Adding routes

--- Controller/TwitterController.php
+++ Controller/TwitterController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tom32i\TwitterBundle\Controller;
+namespace Tom32i\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
